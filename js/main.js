cb0139e595df20977ed0978bb0a3af1e31bc7ce3
petrmart <petrmart@inbox.ru>
2020-12-16 22:12:19 +0300

    addet: mail form and modal window

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+$(document).ready(function () {
 var hotelSlider = new Swiper('.hotel-slider', {
   // Optional parameters
   loop: true,
@@ -32,9 +33,29 @@ var reviewsSlider = new Swiper('.reviews-slider', {
   },
 })
 
-var menuButtom = document.querySelector('.menu-button');
-menuButtom.addEventListener('click', function() {
+var menuButtom = $('.menu-button');
+menuButtom.on('click', function() {
   console.log('Клик по кнопке меню')
-  document.querySelector(".navbar-bottom")
-  .classList.toggle('navbar-bottom--visible');
+  $(".navbar-bottom").toggleClass('navbar-bottom--visible');
+});
+
+var modalButton = $("[data-toggle=modal]");
+var closeModalButton = $(".modal__close");
+modalButton.on("click", openModal);
+closeModalButton.on("click", closeModal);
+
+function openModal(){
+  var modalOverlay = $(".modal__overlay");
+  var modalDialog = $(".modal__dialog");
+  modalOverlay.addClass("modal__overlay--visible");
+  modalDialog.addClass("modal__dialog--visible");
+}
+function closeModal(event){
+  event.preventDefault();
+  var modalOverlay = $(".modal__overlay");
+  var modalDialog = $(".modal__dialog");
+  modalOverlay.removeClass("modal__overlay--visible");
+  modalDialog.removeClass("modal__dialog--visible");
+}
+
 });
---
+++ send.php
@@ -0,0 +1,57 @@
+<?php
+// Файлы phpmailer
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+require 'phpmailer/Exception.php';
+
+// Переменные, которые отправляет пользователь
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+
+// Формирование самого письма
+$title = "Новое обращение Best Tour Plan";
+$body = "
+<h2>Новое обращение</h2>
+<b>Имя:</b> $name<br>
+<b>Телефон:</b> $phone<br><br>
+<b>Сообщение:</b><br>$message 
+";
+
+// Настройки PHPMailer
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+try {
+    $mail->isSMTP();   
+    $mail->CharSet = "UTF-8";
+    $mail->SMTPAuth   = true;
+    // $mail->SMTPDebug = 2;
+    $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
+
+    // Настройки вашей почты
+    $mail->Host       = 'smtp.gmail.com'; // SMTP сервера вашей почты
+    $mail->Username   = 'greekrock.ru@gmail.com'; // Логин на почте
+    $mail->Password   = 'cWiYg8gs8qe5qNb'; // Пароль на почте
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port       = 465;
+    $mail->setFrom('greekrock.ru@gmail.com', 'Пётр Мартовицкий'); // Адрес самой почты и имя отправителя
+
+    // Получатель письма
+    $mail->addAddress('petrmart@inbox.ru');  
+    // $mail->addAddress('youremail@gmail.com'); // Ещё один, если нужен
+
+    // Отправка сообщения
+    $mail->isHTML(true);
+    $mail->Subject = $title;
+    $mail->Body = $body;    
+
+// Проверяем отравленность сообщения
+if ($mail->send()) {$result = "success";} 
+else {$result = "error";}
+
+} catch (Exception $e) {
+    $result = "error";
+    $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+}
+
+// Отображение результата
+header('location: thankyou.html');
